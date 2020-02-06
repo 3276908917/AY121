bc40053e8966aa82ec94f06ec9b36e1766933c86
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-02-06 10:42:59 -0800

    Modularization IV

--- lab1/src/analysis.py
+++ lab1/src/analysis.py
@@ -1,9 +1,14 @@
+# todo: clean up invf and put the old version in review.py,
+    # just like you did with power_plot and raw_power
+# todo: expand power_plot to return values of multiple spikes
+    # this will make it re-usable for the mixed signals section
+
 import ugradio
 import numpy as np
 import matplotlib.pyplot as plt
 
 # I am assuming that we are continuing to use sample frequency 6.25e6 Hz
-def power(sample, rate):
+def raw_power(sample, rate):
     """
     Plot power spectrum of @sample at @rate sampling rate
     and return Fourier transform as well as power array
@@ -14,6 +19,30 @@ def power(sample, rate):
     plt.show()
     return f, P
 
+def mini_power_spectrum(sample, ifreq, norm, srate=6.25e6):
+    """
+    Clean, labeled plot of power spectrum of @sample
+    corresponding to a signal at @ifreq (in MHz)
+    with maximum amplitude @norm (in mV)
+    sampled at rate @srate (default is our value for week 1 data collection)
+    returns SINGLE frequency of largest power spike,
+        so this function does not work as well for
+        combined signals!
+    """
+    fig = plt.figure()
+
+    ax3 = fig.add_subplot(111)
+    f = ugradio.dft.dft(sample, vsamp=srate)
+    P = np.abs(f[1]) ** 2
+    ax3.plot(f[0], normalize(P, norm ** 2))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Power (nV$^2$)')
+    plt.title('Power Spectrum: ' + str(ifreq) + ' MHz sinusoid')
+
+    plt.show()
+	
+    return f[0][np.argmax(P)]
+
 def invf(transform):
     """
     Plot inverse Fourier transform of @transform
@@ -23,6 +52,108 @@ def invf(transform):
     plt.plot(i[0], i[1])
     plt.show()
     return i
+
+def zoom_comparison(sample, cut, freq, norm):
+    """
+    First plot: raw data (should be blocky and only of significance as a whole)
+    Second plot: @cut-sample subsection of data, to make sure it fits expected shape
+    Third plot: power spectrum. Eyeballing is not very helpful, so
+        the function also returns the index associated with the
+        power-maximizing frequency.
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(311)
+
+    ax1.plot(times[:len(sample)], normalize(sample, norm))
+    plt.xlabel('Time (s)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('Voltage vs Time: ' + str(freq) + ' MHz sinusoid')
+
+    ax2 = fig.add_subplot(312)
+    ax2.plot(times[:cut], normalize(sample[:cut], norm))
+    plt.xlabel('Time (s)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('First ' + str(cut) + ' samples: ' + str(freq) + ' MHz sinusoid')
+
+    ax3 = fig.add_subplot(313)
+    f = ugradio.dft.dft(sample, vsamp=6.25e6)
+    P = np.abs(f[1]) ** 2
+    ax3.plot(f[0], normalize(P, norm ** 2))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Power (nV$^2$)')
+    plt.title('Power Spectrum: ' + str(freq) + ' MHz sinusoid')
+
+    plt.show()
+    return f[0][np.argmax(P)]    
+
+# Voltage spectrum
+def volt_spec(sample, ifreq, norm, srate=6.25e6):
+    """
+    Plot 1: real and imaginary components of voltage spectrum of sample.
+    Plot 2: just the real part
+    Plot 3: just the imaginary part
+    @ifreq input frequency
+    @norm maximum observed amplitude of input signal
+    @srate sampling frequency, default is lab 1 week 1
+    
+    Unless there is great overlap between imaginary and real components,
+        use mini_volt_spec instead
+    """
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(311)
+
+    f = ugradio.dft.dft(sample, vsamp=srate)
+
+    ax1.plot(f[0], normalize(np.real(f[1]), norm), label='real component')
+    ax1.plot(f[0], normalize(np.imag(f[1]), norm), label='imaginary component')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('Voltage Spectrum: ' + str(ifreq) + ' MHz sinusoid')
+
+    ax1.legend(bbox_to_anchor=(1, 1))
+
+    ax2 = fig.add_subplot(312)
+
+    ax2.plot(f[0], normalize(np.real(f[1]), norm))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('Real Voltage Spectrum: ' + str(ifreq) + ' MHz sinusoid')
+
+    ax3 = fig.add_subplot(313)
+
+    ax3.plot(f[0], normalize(np.imag(f[1]), norm))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('Imaginary Voltage Spectrum: ' + str(ifreq) + ' MHz sinusoid')
+
+    plt.show()
+
+def mini_volt_spec(sample, ifreq, norm, srate=6.25e6):
+    """
+    Simultaneously plot real and imaginary components of voltage spectrum of sample.
+    @ifreq input frequency
+    @norm maximum observed amplitude of input signal
+    @srate sampling frequency, default is lab 1 week 1
+    
+    If there is excessive overlap between imaginary and real components,
+        use volt_spec instead
+    """
+    
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+
+    f = ugradio.dft.dft(sample, vsamp=srate)
+
+    ax1.plot(f[0], normalize(np.real(f[1]), norm), label='real component')
+    ax1.plot(f[0], normalize(np.imag(f[1]), norm), label='imaginary component')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel(r'Voltage (mV)')
+    plt.title('Voltage Spectrum: ' + str(ifreq) + ' MHz sinusoid')
+
+    ax1.legend(bbox_to_anchor=(1, 1))
+
+    plt.show()
 
 def full_comparison(sample, rate):
     """
@@ -57,36 +188,3 @@ def full_comparison(sample, rate):
     # In conclusion, I think that the manual is suggesting
     # that we take the inverse Fourier transform directly
     # of the Fourier transform (no extra math in between).
-
-def zoom_comparison(sample, cut, freq, norm):
-    """
-    First plot: raw data (should be blocky and only of significance as a whole)
-    Second plot: @cut-sample subsection of data, to make sure it fits expected shape
-    Third plot: power spectrum. Eyeballing is not very helpful, so
-        the function also returns the index associated with the
-        power-maximizing frequency.
-    """
-    fig = plt.figure()
-    ax1 = fig.add_subplot(311)
-
-    ax1.plot(times[:len(sample)], normalize(sample, norm))
-    plt.xlabel('Time (s)')
-    plt.ylabel(r'Voltage (mV)')
-    plt.title('Voltage vs Time: ' + str(freq) + ' MHz sinusoid')
-
-    ax2 = fig.add_subplot(312)
-    ax2.plot(times[:cut], normalize(sample[:cut], norm))
-    plt.xlabel('Time (s)')
-    plt.ylabel(r'Voltage (mV)')
-    plt.title('First ' + str(cut) + ' samples: ' + str(freq) + ' MHz sinusoid')
-
-    ax3 = fig.add_subplot(313)
-    f = ugradio.dft.dft(sample, vsamp=6.25e6)
-    P = np.abs(f[1]) ** 2
-    ax3.plot(f[0], normalize(P, norm ** 2))
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel(r'Power (nV$^2$)')
-    plt.title('Power Spectrum: ' + str(freq) + ' MHz sinusoid')
-
-    plt.show()
-    return f[0][np.argmax(P)]    
