6133f651c7b29b39dc6eb11ef5401c6ae5a85304
3276908917 <lfinkbeiner@berkeley.edu>
2020-02-09 15:55:18 -0800

    Leaving 5.3 for now; will return later

--- lab1/client.py
+++ lab1/client.py
@@ -5,3 +5,11 @@ r1 = redos['re1']
 r1c = (r1[100:]).astype(float)
 p1 = power_plot(r1c, 658) 
 i1 = invf(p1)
+plot(i1[0][600:625], np.real(i1[1][600:625]), r'Time ($\mu$s)', 'Voltage (mV)')   
+n_corr = np.correlate(r1c, r1c, mode='full')  
+p_ACF = mini_volt_spec(n_corr, 658) 
+
+# It seems to me like the FT of the ACF looks like the FT of the input signal.
+# I am struggling to demonstrate either of the following:
+    # Power spectrum looks like FT of ACF
+    # iFT of power spectrum looks like ACF
