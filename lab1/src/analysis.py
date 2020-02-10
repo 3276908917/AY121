2af0e77344d9fe51dfc1c155b1849937b0d197b9
3276908917 <lfinkbeiner@berkeley.edu>
2020-02-10 11:47:54 -0800

    Added histogram maker

--- lab1/src/analysis.py
+++ lab1/src/analysis.py
@@ -8,6 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+def hist_gauss(sample, b, sigma):
+    count, bins, ignored = plt.hist(sample, 64, density=True)
+    plt.plot(bins, 4.5/(sigma * np.sqrt(2 * np.pi)) * np.exp(-bins ** 2 / (2 * sigma ** 2)), linewidth=2, color='r')
+    plt.ylabel(r'Frequency')
+    plt.xlabel('Voltage (V)')
+    plt.show()
+
 def power_plot(sample, norm, srate=6.25e6, ifreq=None, freqs=None):
     """
     Clean, labeled plot of power spectrum of @sample
