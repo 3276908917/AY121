07f22c21c79c43572a1fd7714d12784de68ea635
3276908917 <lfinkbeiner@berkeley.edu>
2020-02-10 12:06:36 -0800

    Added power blocker functionality

--- lab1/src/take_data.py
+++ lab1/src/take_data.py
@@ -25,5 +25,15 @@ def d():
     """
     return ugradio.pico.capture_data('1V', divisor=2, dual_mode=True)
 
+def power_array(n, norm):
+    P = []
+    for c in n:
+        f = ugradio.dft.dft(c, vsamp=62.5e6)    
+        nextP = np.abs(f[1]) ** 2
+        x = f[0] / 10 ** 6
+        y = normalize(nextP, norm ** 2) / 1000 ** 2
+        P.append([x, y])
+    return P
+
 # Here is an example of a labeled save: 
 # np.savez('bundle1', trial1=t1, trial2=t2, trial3=t3, trial4=t4, trial5=t5, trial6_1=t6_1, trial6_2=t6_2, trial6_3=t6_3, trial7=t7, noises=n) 
