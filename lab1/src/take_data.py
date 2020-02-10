6420c3a9d0b9b1c8da3eb8e5264ad56506478d29
3276908917 <lfinkbeiner@berkeley.edu>
2020-02-10 12:36:49 -0800

    Layering the power averages

--- lab1/src/take_data.py
+++ lab1/src/take_data.py
@@ -35,5 +35,37 @@ def power_array(n, norm):
         P.append([x, y])
     return P
 
+# We are assuming here that all elements of the power combo have the same
+   # frequency domain
+def noise_averaged(P, N):
+    avg = P[0][1]
+    for c in range(1, N):
+        avg += P[c][1]
+    avg /= N
+    return avg
+
+def noise_layer:
+    """
+    General but highly volatile plotting function
+    (because I do not like writing functions in the shell).    
+    Mostly ad-hoc and designed for rapid plotting 
+    of similar categories of data.
+    """
+    plt.figure(figsize=(3,3))
+    plt.subplots_adjust(left=.2, bottom=.15, right=.95, top=.9)
+    if dual:
+        plt.plot(x, y[0], label=noteA)
+        plt.plot(x, y[1], label=noteB)
+        plt.legend()
+    else:    
+        plt.plot(x, y)   
+    
+    plt.xlabel(xnote, fontsize=12)
+    plt.ylabel(ynote, fontsize=12)
+    if logv:    
+        plt.yscale('log')	
+
+    plt.show()
+
 # Here is an example of a labeled save: 
 # np.savez('bundle1', trial1=t1, trial2=t2, trial3=t3, trial4=t4, trial5=t5, trial6_1=t6_1, trial6_2=t6_2, trial6_3=t6_3, trial7=t7, noises=n) 
