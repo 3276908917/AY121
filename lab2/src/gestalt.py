5ee161995332badf9f6c0673f9320a9b77aa24b8
Lukas Finkbeiner <lfinkbeiner@ltsp171.ugastro.berkeley.edu>
2020-02-19 12:44:10 -0800

    mean and median functions implemented

--- lab2/src/gestalt.py
+++ lab2/src/gestalt.py
@@ -7,8 +7,19 @@ def d2():
     return ugradio.pico.capture_data('50mV', divisor=1, dual_mode=True)
 
 def complex_combine(real, imag=None, defaults=True):
-	if defaults:
-		r = real[100:16000]
-		i = real[16100:32000]
-		return r + 1j*i
-	return real + 1j*imag
+    if defaults:
+        r = real[100:16000]
+        i = real[16100:32000]
+        return r + 1j*i
+    return real + 1j*imag
+
+# combines power level arrays, chiefly for use with the subsequent
+# average and median methods.
+def power_lists(P, N):
+    stack = [P[i][1] for i in range(1, N)]
+
+def samples_median(power_list):
+    return np.median(power_list, axis=0)
+
+def samples_mean(power_list):
+    return np.mean(power_list, axis=0)
