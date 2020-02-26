61052a01007d733e2bdd395ec5a01b942986cc47
Lukas Finkbeiner <lfinkbeiner@ltsp183.ugastro.berkeley.edu>
2020-02-26 13:05:12 -0800

    Rotations mostly complete; testing has been limited thus far.

--- lab2/src/rotations.py
+++ lab2/src/rotations.py
@@ -1,13 +1,42 @@
 import numpy as np
-import ugradio
+import ugradio.timing
 
-eq2g = np.array([[-.054876, -.873437, -.483835], [.494109, -.444830, .746982], [-.867666, -.198076, .455984]]
+eq2g = np.array([[-.054876, -.873437, -.483835], [.494109, -.444830, .746982], [-.867666, -.198076, .455984]])
 
-eq_to_ha(LST):
-    return np.array([np.cos(LST), np.sin(LST), 0], [-np.sin(LST), np.cos(LST), 0], [0, 0, 1])
+def eq_to_ha(LST=ugradio.timing.lst()):
+    s = np.sin(LST)
+    c = np.cos(LST)
+    return np.array([[c, s, 0], [-s, c, 0], [0, 0, 1]])
 
-topocentric(az, alt):
+def topocentric(az, alt):
     return np.array([np.cos(az) * np.cos(alt), np.sin(az) * np.cos(alt), np.sin(alt)])
 
-# Not yet implemented
-gal_to_topo():
+def ha_to_topo(phi):
+    s = np.sin(phi)
+    c = np.cos(phi)
+    return np.array([[-s, 0, c], [0, -1, 0], [c, 0, s]])
+
+def rct_gal(l, b):
+    return np.array([np.cos(l) * np.cos(b), np.cos(l) * np.sin(b), np.sin(l)])
+
+def gal_to_topo(el, be, lat, radians=False):
+    if not radians:
+        l = np.radians(el)
+        b = np.radians(be)
+        phi = np.radians(lat)
+    else:
+        l = el
+        b = be
+        phi = np.radians(lat)
+    rct = rct_gal(l, b)
+    ra_dec = np.dot(np.linalg.inv(eq2g), rct)
+    hrd = np.dot(np.linalg.inv(eq_to_ha()), ra_dec)
+    topo = np.dot(ha_to_topo(phi), hrd)
+    return new_sphere(topo, radians)
+
+def new_sphere(out_arr, radians=False):
+    gp = np.arctan2(out_arr[1], out_arr[0])
+    tp = np.arcsin(out_arr[2])
+    if not radians:
+        return np.degrees(gp), np.degrees(tp)   
+    return gp, tp
