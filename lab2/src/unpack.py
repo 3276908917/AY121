b89b466f7e2d254a7fc331f115bc6c12846c9607
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-02-27 11:35:52 -0800

    Major documentation upgrade

--- lab2/src/unpack.py
+++ lab2/src/unpack.py
@@ -2,7 +2,18 @@
 offset = 1600000
 sample_size = 16000
 
+# It's called bit because we are operating on a single block
+# i.e. pico-sampler concatenates samples into one giant array.
 def bit(chunk):
+    '''
+    It is called bit because it is to be used on one block at a time.
+
+    Return an array, equal in length to the number of blocks,
+    of samples (arrays of complex voltages).
+
+    For customization of the functionality, see global parameters
+    in the header.
+    '''
     c = []
     for i in range(0, offset, sample_size):
         real = chunk[i: i + sample_size]
@@ -12,6 +23,16 @@ def bit(chunk):
 
 # e.g. off = load_saves('../on.npz')['raw_off']
 def pipeline(case, reduction):
+    '''
+    Given a collection of blocks (pico-sampler concatenated samples),
+    we turn each block into a group of arrays (in complex-conjugate
+    format), then we perform the power spectrum on all arrays in that
+    block, then we perform the reduction algorithm on each group
+    of complex arrays, then we perform the reduction on all blocks.
+
+    Be careful about using this function in low-memory environments!
+    You would either kill or be killed by the OS.
+    '''
     compressor = []
     for a in case:
         c_chunk = bit(a)
@@ -19,7 +40,14 @@ def pipeline(case, reduction):
         compressor.append(reduction(P))
     return reduction(compressor)
 
+# This helped me with Kyle's data.
 def thermal():
+    '''
+    Assumes all files in current directory are data files,
+        of the form: 1 float per line
+    Copies the directory into an array, where each element
+        is an array describing the floats contained within one file.
+    '''
     block = []
     for file in glob.glob("./*"):
         samples = []
@@ -30,6 +58,8 @@ def thermal():
         block.append(samples)
     return block
 
+# This helped me with Max's data.
+# It may help again in the future, in case I shift to pickle format.
 def unpickle_folder():
     re_block, im_block = [], []
     for file in glob.glob("./*"):
