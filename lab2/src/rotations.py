3715846cebd5689534ea01ac261fc46c94c17cdb
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-03-01 14:30:36 -0800

    All Work thus Far Completed in Documentation

--- lab2/src/rotations.py
+++ lab2/src/rotations.py
@@ -4,25 +4,44 @@
 import numpy as np
 import ugradio.timing
 
-eq2g = np.array([[-.054876, -.873437, -.483835], [.494109, -.444830, .746982], [-.867666, -.198076, .455984]])
+# The change-of-basis matrix between equatorial and galactic coordinate systems
+eq_to_gal = np.array([[-.054876, -.873437, -.483835], [.494109, -.444830, .746982], [-.867666, -.198076, .455984]])
 
 def eq_to_ha(LST):
+    '''
+    Return the change-of-basis matrix between the equatorial and
+    hour angle declination coordinate systems.
+    The conversion depends on the @LST, Local Siderial Time
+    '''
     s = np.sin(LST)
     c = np.cos(LST)
     return np.array([[c, s, 0], [s, -c, 0], [0, 0, 1]])
 
-#def topocentric(az, alt):
-#    return np.array([np.cos(az) * np.cos(alt), np.sin(az) * np.cos(alt), np.sin(alt)])
-
 def ha_to_topo(phi):
+    '''
+    Return the change-of-basis matrix between the hour angle declination
+    and topocentric coordinate systems.
+    The conversion depends on the user's current latitude @phi,
+        which must be given in radians.
+    '''
     s = np.sin(phi)
     c = np.cos(phi)
     return np.array([[-s, 0, c], [0, -1, 0], [c, 0, s]])
 
 def rct_gal(l, b):
+    '''
+    Given a pair of angles @l, @b (both angles must be in radians),
+    return the corresponding 3x1 rectangular vector.
+    '''
     return np.array([np.cos(b) * np.cos(l), np.cos(b) * np.sin(l), np.sin(b)])
 
 def gal_to_topo(el, be, lat, radians=False):
+    '''
+    @radians determines the format of BOTH input and output!
+    Given a pair of angles @el and @be (in galactic coordinates),
+    return a pair of angles relating the associated
+    azimuth and altitude.
+    '''
     if not radians:
         l = np.radians(el)
         b = np.radians(be)
@@ -32,7 +51,7 @@ def gal_to_topo(el, be, lat, radians=False):
         b = be
         phi = np.radians(lat)
     rct = rct_gal(l, b)
-    ra_dec = np.dot(np.linalg.inv(eq2g), rct)
+    ra_dec = np.dot(np.linalg.inv(eq_to_gal), rct)
     # The program at least works fine up until here
     hrd = np.dot(np.linalg.inv(eq_to_ha(ugradio.timing.lst())), ra_dec)
     topo = np.dot(ha_to_topo(phi), hrd)
@@ -40,6 +59,11 @@ def gal_to_topo(el, be, lat, radians=False):
     return new_sphere(topo, radians)
 
 def new_sphere(out_arr, radians=False):
+    '''
+    Given a 3x1 vector,
+    return the corresponding pair of angles
+    @radians determines whether the angles are given in radians.
+    '''
     gp = np.arctan2(out_arr[1], out_arr[0])
     tp = np.arcsin(out_arr[2])
     if not radians:
