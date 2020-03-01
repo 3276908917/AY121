46cacb684dccd41b89485c42c407e344196c7fe7
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-02-29 22:00:54 -0800

    Documentation on data taking functions; deprecated methods trashed

--- lab2/src/data_taking.py
+++ lab2/src/data_taking.py
@@ -1,53 +1,28 @@
-import time
-
 # vertical angle difficult to measure: plus or minus 10 degrees,
 # which is already the angular resolution, probably of the big horn
 
 def d2():
-    """
+    '''
     Abbreviation function: dual get hard-coded for week 1 data collection.
     Acquire two streams of data through pico sampler channels A and B.
     50 mV range, divisor=6
-    """
+    '''
     pack = ugradio.pico.capture_data('50mV', divisor=6, nsamples=16000, dual_mode=True)
     pack.shape = (2, -1, 16000)
     return pack
 
 # recommended: loops=100
 def d200(loops):
+    '''
+    Abbreviation function: capture pico-sampler dual channel data,
+    at 50 mV range, with divisor 6, where each increment of
+    @loops corresponds to an additional order of 100 blocks of data.
+    E.g. loops=100 -> 10k samples returned
+
+    Progress bar is hard-coded for loops=100
+    '''
     data_chunk = []
     for i in range(loops):
         data_chunk.append(ugradio.pico.capture_data('50mV', divisor=6, dual_mode=True, nblocks=100))
         print(str(i + 1) + '% complete')
     return data_chunk
-
-# deprecated
-def complex_combine(real, imag=None):
-    if imag is None:
-        r = real[100:16000]
-        i = real[16100:32000]
-        return r + 1j*i
-    return real + 1j*imag
-
-# deprecated
-def data_to_comp(glob, sample_size=16000):
-    complex_combo = []    
-    #samples_per_block = len(glob[0][0])
-    offset = len(glob[0]) // 2
- #   num_pairs = offset / sample_size
-
-    for c in glob:
-        for i in range(0, offset, sample_size):
-            real = c[i: i + sample_size]
-            imag = c[offset + i: offset + i + sample_size]
-            complex_combo.append(real + 1j*imag)
-
-    return complex_combo
-
-# deprecated
-def complex_bblock(glob):
-    c = []
-    offset = len(glob[0]) // 2
-    for a in glob:
-        c.append(a[0:offset] + 1j*a[offset:])
-    return c
