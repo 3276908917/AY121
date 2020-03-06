0d8acee2622c2f925d870a4f9b29240c564737c1
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-03-05 22:22:44 -0800

    I think all of the notebooks are fixed
    Unfortunately, Calibration_Analysis_Demonstration is not testable

--- lab2/synthesis.py
+++ lab2/synthesis.py
@@ -1,14 +1,14 @@
 import os
 
-os.chdir(r"../lab1")
+os.chdir(r"./../lab1")
 exec(open('synthesis.py').read())
 
-os.chdir(r"../lab2/src")
-exec(open('data_taking.py').read())
-exec(open('data_appraisal.py').read())
-exec(open('analysis2.py').read())
-exec(open('rotations.py').read())
-exec(open('unpack.py').read())
-exec(open('chi.py').read())
+os.chdir(r"./../lab2/src")
+exec(open('./data_taking.py').read())
+exec(open('./data_appraisal.py').read())
+exec(open('./analysis2.py').read())
+exec(open('./rotations.py').read())
+exec(open('./unpack.py').read())
+exec(open('./chi.py').read())
 
-os.chdir(r"../")
+os.chdir(r"./../")
