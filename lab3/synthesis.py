9b67f08fe026608807f3311bfc61d7e94e7d3ff9
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-03-19 13:19:52 -0700

    Directory swapping is now corrected

--- lab3/synthesis.py
+++ lab3/synthesis.py
@@ -2,13 +2,14 @@
 
 import os
 
-os.chdir(r"../../lab1")
+os.chdir(r"../lab1")
 exec(open('synthesis.py').read())
 
 os.chdir(r"../lab2")
 exec(open('synthesis.py').read())
 
-os.chdir(r"src")
+os.chdir(r"../lab3")
+#os.chdir(r"src")
 # Code files go here
 
-os.chdir(r"./../")
+#os.chdir(r"./../")
