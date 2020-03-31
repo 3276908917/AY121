6e4045ed4a7a8dd0881122c47918bb58bc6f8b17
Otto von Bismarck <lfinkbeiner@berkeley.edu>
2020-03-30 23:39:13 -0700

    [WIP] Some rough estimates as to when sources will be available
    This code is incomplete for two reasons:
    1. if the source is already visible it does not work. An additional consequence of this is that simply increasing the start time will not give the NEXT boundary after the rise.
    2. we increment by minutes, because seconds are just too slow. Ideally I would use something like a binary search, with the step halving and flipping in sign every time we overshoot.

--- lab3/lukas/src/utils.py
+++ lab3/lukas/src/utils.py
@@ -1,5 +1,9 @@
 import ugradio
 
+# Cygnus A "Right Ascension" and Declanation
+CAR = ha(19, 59, 28.3566)
+CAD = dec(40, 44, 2.096)
+
 def bounds_pst(stamp_array):
     '''
     Displays (does not return) the start time and stop time for one
@@ -12,3 +16,25 @@ def bounds_pst(stamp_array):
     end_ux = stamp_array[:, 2][len(stamp_array) - 1]
     end_pst = ugradio.timing.local_time(end_ux)
     print('Last datum collected at approximately:\t' + end_pst)
+
+def next_time_boundary(pos_func, start=ugradio.timing.unix_time()):
+    alt, az = pos_func(ugradio.timing.julian_date(start))
+    add_sec = 1
+    while alt < 5 or alt > 175:
+        alt, az = pos_func(ugradio.timing.julian_date(start + add_sec))
+        add_sec += 60
+    return ugradio.timing.local_time(start + add_sec)
+
+def moon_altaz(j_date):
+    ra_moon, dec_moon = ugradio.coord.moonpos(jd=j_date)
+    return ugradio.coord.get_altaz(ra_moon, dec_moon, jd=j_date)
+
+def sun_altaz(j_date):
+    ra_sun, dec_sun = ugradio.coord.sunpos(jd=j_date)
+    return ugradio.coord.get_altaz(ra_sun, dec_sun, jd=j_date)
+
+def star_altaz(old_ra, old_dec):
+    def stargazer(j_date):
+        prec_ra, prec_dec = ugradio.coord.precess(old_ra, old_dec, jd=j_date)
+        return ugradio.coord.get_altaz(prec_ra, prec_dec, jd=j_date)
+    return stargazer
