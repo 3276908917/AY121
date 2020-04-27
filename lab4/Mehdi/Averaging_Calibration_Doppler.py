bd07d9b68d2891c37f2af34e98f784601eb5038f
Marhror <30963977+Marhror@users.noreply.github.com>
2020-04-27 02:29:35 -0700

    Add files via upload

--- lab4/Mehdi/Averaging_Calibration_Doppler.py
+++ lab4/Mehdi/Averaging_Calibration_Doppler.py
@@ -171,6 +171,7 @@ def Frequency_spectrum(header, spectrum_data):
 def base_fit_average(average_spectra, frequency):
 	
 	"""	
+	
 	Filter our average data, then use 1d interpolation on the data within boundaries 
 	
 	"""
@@ -204,8 +205,9 @@ def doppler_velocity_correction(header, frequency, velocity_correction='off'):
 	
 	We use the header from the collected data each time to get the ra, dec and jd; and use them to get the doppler correction, then we get the doppler velocity	
 	"""
+	
 	#parameters
-	HI_freq = 1420.405751  # frequency of our HI signal in Mhz
+	HI_freq = 1420.40575  # frequency of our HI signal in Mhz
 	c = 3e5  #speed of light in km/s 
 		
 		
@@ -227,7 +229,8 @@ def doppler_velocity_correction(header, frequency, velocity_correction='off'):
 		Doppler_correction = ugradio.doppler.get_projected_velocity(ra, dec, jd) 
 
 		print('Doppler_correction',Doppler_correction)
-		Velocity =  np.array(doppler_velocity) - Doppler_correction.value
+		
+		Velocity =  np.array(doppler_velocity) - (Doppler_correction.value/1e3) #convertion Doppler correction from m/s to km/s
 	
 	#turn off Doppler correction
 	elif velocity_correction=='off':
@@ -237,7 +240,7 @@ def doppler_velocity_correction(header, frequency, velocity_correction='off'):
 
 
 
-def Gain(signal_noise_on, signal_noise_off, number_spectra, Noise_temp, Sky_temp):
+def Gain(signal_noise_on, signal_noise_off, number_spectra=10, Noise_temp=301, Sky_temp=1):
 	
 	"""
 	Calculate the Gain (G) like in Lab 2, uses the signal with and without noise, temperature of the sky and temprature from the noise in kelvin
@@ -248,14 +251,10 @@ def Gain(signal_noise_on, signal_noise_off, number_spectra, Noise_temp, Sky_temp
 	
 	noise_off_spectra = Average_spectrum(signal_noise_off, number_spectra )
 	
-	
-
 #	Noise_temp - Sky_temp = 300 #kelvin
 
 	gain = ((Noise_temp - Sky_temp) / np.sum(noise_on_spectra - noise_off_spectra)) * np.sum(noise_off_spectra)
-	
-	print('Gain',gain)
-	
+		
 	return gain
 
 
@@ -263,16 +262,15 @@ def Gain(signal_noise_on, signal_noise_off, number_spectra, Noise_temp, Sky_temp
 	
 
 def final_calibration(file_name, Polarization_number='first', Gain=1, velocity_correction='off' ):
+		
+	"""
+	- Calculate the gain first Gain(noise_on, noise_off, Noise_temp, Sky_temp) then use the final value of the gain for the calibration
 	
+	- Calibrate the leushner data, change it from frequency Mhz vs amplitude V^2 to temperature spectra (kelvin) with respect to doppler velocity m/s (LSR frame)
 	
 	"""
-	-Calculate the gain first Gain(noise_on, noise_off, Noise_temp, Sky_temp) then use the final value of the gain for the calibration
 	
-	-Calibrate the leushner data, change it from frequency Mhz vs amplitude V^2 to temperature spectra (kelvin) with respect to doppler velocity m/s (LSR frame)
-	
-	"""
-	
-# 	gain = 300 # will calculate it later once
+	gain = 300 # will calculate it later once
 	
 	leushner_data = Read_leushner_header(file_name)
 		
@@ -293,33 +291,58 @@ def final_calibration(file_name, Polarization_number='first', Gain=1, velocity_c
 	doppler_velocity = doppler_velocity_correction(header, freq, velocity_correction)
 	
 	
-	return doppler_velocity, Temperature
+	return doppler_velocity, Temperature 
 	
 	
 
-fon= fits.open('thirty_plane_on.fits')
+fon= fits.open('attempt_all_150.0_degrees_635MHz_noisy.fits')
 N = fon[0].header['NSPEC']
 	
 
-foff= fits.open('thirty_plane_off.fits')
+foff= fits.open('attempt_all_150.0_degrees_635MHz_quiet.fits')
 N = foff[0].header['NSPEC']
 
-
+print (foff[0].header)
 gain = Gain(fon, foff, N, 301, 1)
 #calculate the gain:
-print ('Gain', gain)
+print ('Gain', Gain(fon, foff, N, 301, 1))
 
 
 #comparing on/off data, by inputing file name and polarization number 
 
-plot_average('thirty_plane_on.fits')
-plot_average('thirty_plane_off.fits')
-plt.show()
+#plot_average('twenty_plane_on.fits')
+#plot_average('twenty_plane_off.fits')
+#plt.show()
+#
+#plot_average('twenty_plane_on.fits','second')
+#plot_average('twenty_plane_off.fits','second')
+#plt.show()
 
-plot_average('thirty_plane_on.fits','second')
-plot_average('thirty_plane_off.fits','second')
-plt.show()
 
+
+#comparing on/off data, by inputing file name and polarization number 
+
+#plot_average('attempt_all_160.0_degrees_635MHz_noisy.fits')
+#plot_average('attempt_all_160.0_degrees_634MHz_noisy.fits')
+
+#plot_average('attempt_all_160.0_degrees_635MHz_quiet.fits')
+#plot_average('attempt_all_160.0_degrees_634MHz_quiet.fits')
+#
+#plot_average('attempt_all_160.0_degrees_635MHz_quiet.fits', 'second')
+#plot_average('attempt_all_160.0_degrees_634MHz_quiet.fits', 'second')
+
+
+#plot_average('attempt_all_160.0_degrees_635MHz_noisy.fits')
+#plot_average('attempt_all_160.0_degrees_634MHz_noisy.fits')
+
+#plot_average('attempt_all_160.0_degrees_635MHz_noisy.fits', 'second')
+#plot_average('attempt_all_160.0_degrees_634MHz_noisy.fits', 'second')
+#plt.show()
+
+#plot_average('attempt_all_160.0_degrees_634MHz_noisy.fits','second')
+#plot_average('attempt_all_160.0_degrees_635MHz_noisy.fits','second')
+
+#plt.show()
 
 #plot all rawa data from file
 #Plot_leushner_raw_data('thirty_plane_on.fits')
@@ -330,8 +353,136 @@ plt.show()
 
 #calibration, (ugradio.doppler.get_projected_velocity(ra, dec, jd)) outputs the doppler velocity and the unit, this format blocks the velocity correction calculation when it's on.
 
-data = final_calibration('thirty_plane_on.fits', velocity_correction='off', Gain= -1*gain)
-plt.plot(data[1], data[0])
-plt.xlabel('Doppler velocity (km/s)')
-plt.ylabel('Temperature (kelvin)')
-plt.show()
+#data = final_calibration('attempt_all_160.0_degrees_635MHz_quiet.fits', velocity_correction='off', Gain= 2000)
+#plt.plot(data[0], data[1], label='No Doppler correction')
+#plt.xlabel('Doppler velocity (km/s)')
+#plt.ylabel('Temperature (kelvin)')
+#plt.legend()
+#plt.show()
+
+def plot_temperature_doppler(data, velocity_correction= 'off'):
+	
+	
+	data_x = data[0]
+	data_y = data[1]			
+	
+	plt.plot(data_x, data_y, label=' Doppler correction is '+ velocity_correction)
+	
+	plt.xlabel('Doppler velocity (km/s)')
+	plt.ylabel('Temperature (kelvin)')
+	plt.legend()
+	
+
+
+def plot_all_attempts(lo_freq1, lo_freq2, file_name='attempt_all', velocity_correction='off', Gain= 2000 ):
+		
+	degree_spacing = 2/2
+
+	lontitude_min= -10
+	longtitude_max = 250
+	Longtitude_Spacing = (longtitude_max - (lontitude_min)) / degree_spacing
+	
+	longitude_range = np.linspace(lontitude_min, longtitude_max, int(Longtitude_Spacing)+1)
+	 
+	print ('longitude_range',len(longitude_range))
+		
+	Missing_longitudes_values = np.array([])			
+	
+	for l in longitude_range:
+		
+		plt.ion()	
+		
+		try:
+			
+			
+			Lo1_noisy_name = file_name + '_' + str(float(int(l))) +'_degrees_'+ str(lo_freq1) +'MHz_noisy.fits'	
+			Lo1_quiet_name = file_name + '_' + str(float(int(l))) +'_degrees_'+ str(lo_freq1) +'MHz_quiet.fits'
+			
+			Lo2_noisy_name = file_name + '_' + str(float(int(l))) +'_degrees_'+ str(lo_freq2) +'MHz_noisy.fits'	
+			Lo2_quiet_name = file_name + '_' + str(float(int(l))) +'_degrees_'+ str(lo_freq2) +'MHz_quiet.fits'		
+			
+						
+			print (Lo1_noisy_name)	
+			print (Lo1_quiet_name)
+			
+			Lo1_on = Read_leushner_header(str(Lo1_noisy_name))
+			Lo1_off = Read_leushner_header(str(Lo1_quiet_name))
+			
+			Lo2_on = Read_leushner_header(str(Lo2_noisy_name))
+			Lo2_off = Read_leushner_header(str(Lo2_quiet_name))
+			
+			
+			gain_lo1 = 300 / np.sum(Average_spectrum(Lo1_on, N=10, polarization_number='first') - Average_spectrum(Lo1_off, N=10, polarization_number='first')) * np.sum(Average_spectrum(Lo1_off, N=10, polarization_number='first'))
+			
+			gain_lo2 = 300 / np.sum(Average_spectrum(Lo1_on, N=10, polarization_number='first') - Average_spectrum(Lo1_off, N=10, polarization_number='first')) * np.sum(Average_spectrum(Lo1_off, N=10, polarization_number='first'))
+			
+			
+#			print (Average_spectrum(Lo1_on, N=10, polarization_number='first'))
+#			print (Average_spectrum(Lo1_off, N=10, polarization_number='first'))
+			
+		
+
+
+#			Lo1_on = fits.open(str(Lo1_noisy_name))
+#			print('Lo1_on', type(Lo1_on))
+#
+#			Lo1_off  = fits.open(str(Lo1_quiet_name))	
+#			print('Lo1_off'	,  Lo1_off)
+					
+#			fon= fits.open(Lo1_on)
+			N = Lo1_on[0].header['NSPEC']
+			print (N)
+		
+
+
+			
+			data_lo1_on = final_calibration(Lo1_noisy_name, Polarization_number='first', Gain= gain_lo1, velocity_correction='off' )
+			data_lo1_off = final_calibration(Lo1_quiet_name, Polarization_number='first', Gain= gain_lo1, velocity_correction='off' )
+			
+			data_lo2_on = final_calibration(Lo2_noisy_name, Polarization_number='first', Gain= gain_lo2, velocity_correction='off' )
+			data2_lo2_off = final_calibration(Lo2_quiet_name, Polarization_number='first', Gain= gain_lo2, velocity_correction='off' )
+			
+			
+			#Plot average plots of all data in the file within the longtitude we collected
+#			plot_average(Lo1_noisy_name, polarization_number= 'first')
+
+			#Plot Temperatude vs Doppler velocity of all data in the file within the longtitude we collected
+			plot_temperature_doppler(data_lo1_on, velocity_correction= 'off')
+			plot_temperature_doppler(data_lo2_on, velocity_correction= 'off')
+			
+			
+			plt.title('Longtitude'+ str(l))
+			
+
+
+		
+			plt.pause(.1)
+			plt.draw()
+			plt.clf()
+			
+			
+		except:
+			print('Data is not taken for this longtitude')
+
+			Missing_longitudes_values= np.append(Missing_longitudes_values, float(l))
+	
+	plt.ioff()	
+	np.save("missing_longitudes_values", Missing_longitudes_values)
+
+				
+plot_all_attempts(634, 635, file_name = 'attempt_all')	
+
+#plot_average('attempt_all_145.0_degrees_634MHz_noisy.fits', polarization_number= 'first' )
+#plt.show()	
+
+	
+
+
+
+#data = final_calibration('attempt_all_160.0_degrees_635MHz_quiet.fits', velocity_correction='on', Gain= gain)
+#plt.plot(data[0], data[1], label=' Doppler corrected' )
+#plt.xlabel('Doppler velocity (km/s)')
+#plt.ylabel('Temperature (kelvin)')
+#plt.legend()
+#plt.show()
+#
