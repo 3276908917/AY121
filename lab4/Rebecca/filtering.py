e61ed6ea6f467f24a66e5291c2c3a626a10ce995
RebeccaGore <60719931+RebeccaGore@users.noreply.github.com>
2020-04-28 19:18:46 -0700

    Create filtering.py

---
+++ lab4/Rebecca/filtering.py
@@ -0,0 +1,114 @@
+import ugradio 
+import numpy as np
+from astropy.io import fits
+
+def getline(file1,file2):
+    '''Returns the callibrated spectrum for a pointing at a single LO frequency
+
+    Inputs:
+    file1: The name of the data file without noise as a string
+    file2: The name of the data file with noise as a string
+
+    Outputs:
+    gainave * meanoff: The callibrated spectrum for one LO frequency.'''
+    f_noise = fits.open(file1)
+    f_quiet = fits.open(file2)
+    
+    on0,off0 = [],[]
+    on1,off1 = [],[]
+
+    on,off = [],[]
+    for i in range(1,10):
+        on0.append(f_noise[i].data['auto0_real'])
+        off0.append(f_quiet[i].data['auto0_real'])
+
+        on1.append(f_noise[i].data['auto1_real'])
+        off1.append(f_quiet[i].data['auto1_real'])
+
+        on.append(f_noise[i].data['auto0_real'])
+        on.append(f_noise[i].data['auto1_real'])
+        off.append(f_quiet[i].data['auto0_real'])
+        off.append(f_quiet[i].data['auto1_real'])
+
+    meanon0 = np.mean(on0,axis=0)
+    meanoff0 = np.mean(off0,axis=0)
+
+    meanon1 = np.mean(on1,axis=0)
+    meanoff1 = np.mean(off1,axis=0)
+
+    meanon = np.mean(on,axis=0)
+    meanoff = np.mean(off,axis=0)
+
+    gain0 = float(sum(meanoff0) * 80 / (sum(meanon0-meanoff0)))
+    gain1 = float(sum(meanoff1) * 270 / (sum(meanon1-meanoff1)))
+    gainave = .5*(gain0 + gain1)
+
+    return gainave * meanoff
+
+def filterline(line):
+    '''Takes off a third degree polynomial from the baseline of the data
+
+    Inputs:
+    line: The spectrum needing to be filtered
+
+    Outputs:
+    line-baseline: The filtered spectrum'''
+    x = np.array(range(len(line)))
+    keepx = []
+    keepline = []
+    for i in range(0,5000):
+        keepx.append(x[i])
+        keepline.append(line[i])
+    for k in range(6000,len(line)):
+        keepx.append(x[i])
+        keepline.append(line[i])
+    base = np.polyfit(x,line,3)
+    baseline = base[0]*x*x*x + base[1]*x*x + base[2]*x + base[3]
+    return line-baseline
+
+def getmeta(filename):
+    '''Extracts the angle l, the right ascension, the declination, and the julian date of a data set
+
+    Inputs:
+    filename: The name of the data file as a string
+
+    Outputs:
+    l = Galactic longitude of data set
+    ra = Right ascension of data set
+    dec = Declination of data set
+    jd = Julian date of data set'''
+    f = fits.open(filename)
+    dict(f[0].header)
+    l =f[0].header[18]
+    ra = f[0].header[20]
+    dec = f[0].header[21]
+    jd = f[0].header[22]
+    return l, ra, dec, jd
+
+def getfinal(file1,file2,file3,file4):
+    '''Returns a filtered spectrum that accounts for two LO frequencies
+
+    Inputs:
+    file1: The name of the data file without noise for the first LO frequency as a string
+    file2: The name of the data file with noise for the first LO frequency as a string
+    file3: The name of the data file without noise for the second LO frequency as a string
+    file4: The name of the data file with noise for the second LO frequency as a string
+
+    Outputs:
+    l: Average galactic longitude between file1 and file3
+    ra: Average right ascension between file1 and file3
+    dec = Average declination between file1 and file3
+    jd = Average julian date between file1 and file3
+    final_line = The final spectrum after callibrations, filtering, and frequency switching'''
+    line1 = getline(file1,file2)
+    line2 = getline(file3,file4)
+    filtered1 = filterline(line1)
+    filtered2 = filterline(line2)
+    final_line = filtered1-filtered2
+    l1,ra1,dec1,jd1 = getmeta(file1)
+    l2,ra2,dec2,jd2 = getmeta(file3)
+    l = .5*(l1+l2)
+    ra = .5*(ra1+ra2)
+    dec = .5*(dec1+dec2)
+    jd = .5*(jd1+jd2)
+    return l,ra,dec,jd,final_line
